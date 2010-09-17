c95e54d2352c06161531fa1e0bbaacd253a8b576
Kyle Mathews <mathews.kyle@gmail.com>
2010-09-17 14:54:44 -0600

    Added new feature, eduglu_captcha which adds a math captcha form to the user registration page to prevent spammers from creating accounts.

--- eduglu_about/eduglu_about.js
+++ eduglu_about/eduglu_about.js
@@ -1,4 +1,4 @@
 Drupal.behaviors.edugluAboutMembersStripping = function(context) {  
-$(".view-eduglu-about-members tr").mouseover(function(){$(this).addClass("over");}).mouseout(function(){$(this).removeClass("over");});
+  $(".view-eduglu-about-members tr").mouseover(function(){$(this).addClass("over");}).mouseout(function(){$(this).removeClass("over");});
   $(".view-eduglu-about-members tr:odd").addClass("striped");
 }
